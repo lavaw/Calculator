e5aed12801282cdac803219ce4351239475ca77a
lavaw <ronaldo679@gmail.com>
2023-10-21 03:02:42 +0300

    New Model file for calculation method

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rudolf Amiryan
+//  Copyright © 2023
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var  displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            return number
+        } 
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+       
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue = displayValue * -1
+            } else if calcMethod == "%" {
+                displayValue = displayValue / 100
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            }
+        }
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+    
+    }
+
+}
+
---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Rudolf Amiryan on 21.10.23.
+//  Copyright © 2023 London App Brewery. All rights reserved.
+//
+
+import Foundation
