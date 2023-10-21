b40d39f4b601f57f361ad9c945f550a3308d8c71
lavaw <ronaldo679@gmail.com>
2023-10-21 17:41:32 +0300

    Added MVC Refactoring code Practice private access levels and using tuples and switches

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,20 +10,51 @@ import Foundation
 
 struct CalculatorLogic {
     
-    var number: Double
+   private var number: Double?
     
-    init(number: Double) {
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
     
-        if symbol == "+/-" {
-            return number * -1
-        } else if symbol == "%" {
-            return number / 100
-        } else if symbol == "AC" {
+        if let n = number {
+        switch symbol {
+        case "+/-":
+            return n * -1
+        case "%":
+            return n / 100
+        case "AC":
             return 0
+        case "=":
+            return performTwoNumberCalculation(n2: n)
+        default:
+            intermediateCalculation = (n1: n, calcMethod: symbol)
+            
+        }
+    }
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1,
+           let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases")
+            }
         }
         return nil
     }
